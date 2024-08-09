6e1effbce7c45b9bbc4a48fb3cc8abac9f99b10a
TANISHQ CHAUHAN <121804467+MonkTanishq@users.noreply.github.com>
2024-08-09 14:34:11 +0530

    Datatypes in Javascript

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3 + 3) //we are using Nodejs not browser
+
+let name = "hello" //string
+let age = 10 //number
+let isLoggedIn = true //boolean
+
+//Premitive Data Types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value (empty value)
+// undefined => value not assigned
+// symbol => to find uniqueness
+
+//object
+
+console.log(typeof undefined) //undefined
+console.log(typeof null) //object
+
