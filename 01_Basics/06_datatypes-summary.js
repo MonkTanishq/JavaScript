c63be0a155d0e090f5b82d9a3dc6723229aa7c5a
TANISHQ CHAUHAN <121804467+MonkTanishq@users.noreply.github.com>
2024-08-10 19:12:43 +0530

    Datatype Summary

---
+++ 01_Basics/06_datatypes-summary.js
@@ -0,0 +1,38 @@
+//Javascript is a Dynamically Typed Language.
+
+/*Primitive DataTypes
+
+    7 types: String, Number, Boolean, null, undefined, Symbol, BigInt
+
+ 
+
+    //SYMBOL
+    // const id = Symbol("123")
+    // const anotherID = Symbol("123")
+    // console.log(id === anotherID); => false
+    // console.log(id == anotherID); => false
+
+    //BIGINT
+    //const bigNumber = 8428497924797492479472949n
+
+*/
+
+/*Non-Primitive DataTypes
+
+    Array, Objects, Functions
+
+    //Array
+    //const heros = ["hulk", "ninjas", "rangers"]
+
+    //Object
+    //let myObj = {
+                    name: "Piter", 
+                    age: 23
+                  }
+
+    //Function
+    //const myFunction = function(){
+                                    console.log("Hello World")
+                                   }
+
+*/
